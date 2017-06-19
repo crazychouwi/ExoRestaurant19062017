ca7304fa948c0143b559121afa7c24ef492a0de2
Sandra <ptitbou.d_avril@hotmail.fr>
2017-06-19 23:06:26 +0200

    création formulaire ajout de plat, recuperation des données des champs du formulaire et insertion en base

---
+++ config/formPlat.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Formulaire d'ajout de Plats </title>
+  </head>
+  <body>
+    <h1>Formulaire d'ajout de plats</h1>
+    <form class="plats" action="connection.php" method="post">
+    <p><label for="plat">Entrez le nom de votre plat:</label><input type="text" name="plat" value=""></p>
+    <p><label for="prix">Entrez le prix de votre plat:</label><input type="text" name="prix" value=""></p>
+    <p><label for="">Insérez le lien de votre image ici:</Label><input type="text" name="url" value=""></p>
+    <p><input type="submit" value="OK"></p>
+    </form>
+  </body>
+</html>
