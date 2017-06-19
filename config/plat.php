e9816fd57c50768060676bcb98314dbcd3e60874
Sandra <ptitbou.d_avril@hotmail.fr>
2017-06-20 00:03:05 +0200

    recuperation en base de données et affichage de la liste des plats dans plat.php

--- config/plat.php
+++ config/plat.php
@@ -1,1 +1,34 @@
 <!-- Afficher tous les plats -->
+<html>
+	<head>
+	<title>liste des plats </title>
+	</head>
+	<body>
+
+    <h1>Liste des Plats</h1>
+	<?php
+	// Connection à la base
+	mysql_connect("localhost", "root", "matt2610") or die (mysql_error ());
+
+	// Selection de la base
+	mysql_select_db("restaurant") or die(mysql_error());
+
+	// requete SQL
+	$strSQL = "SELECT * FROM plat";
+
+	// Execute la requete (la variable $ contient le résultat)
+	$resultat = mysql_query($strSQL);
+
+	//boucle dans la colonne 'nom' et récupère chaque résultat
+	while($colonne = mysql_fetch_array($resultat)) {
+
+	   // Ecrit la valeur de la colonne 'nom'
+	  echo $colonne['nom'] . "<br />";
+
+	  }
+
+	// Close the database connection
+	mysql_close();
+	?>
+	</body>
+	</html>
